eab28f0e0d2dcb118152e35b8e0f1449f262b727
Bryan C~ <bryan.catiche@gmail.com>
2022-09-28 16:35:58 +0200

    POST

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.get('/entreprises', (req, res) =>{
     res.status(200).json(entreprises)
 })
 
+app.get('/entreprises', (req,res) => {res.status(200).json(entreprises)})
+app.get('/entreprises/:id', (req,res) => {
+    const id = parseInt(req.params.id) 
+    const entreprise = entreprises.find(entreprise => entreprise.id === id)
+    res.status(200).json(entreprise)})
+
+app.post('/entreprises', (req,res) => {
+    entreprises.push(req.body)
+    res.status(200).json(entreprises)})
+
 app.listen(PORT, () =>
     console.log(`En route sur ${HOST}:${PORT}`)
 )
